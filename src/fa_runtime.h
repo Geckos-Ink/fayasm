ee67e6cc387153499c2e252c37d4f8db88f6da67
Riccardo Cecchini <rcecchini.ds@gmail.com>
2025-04-01 23:57:51 +0200

    data flow

--- src/fa_runtime.h
+++ src/fa_runtime.h
@@ -4,12 +4,19 @@
 
 #define FA_JOB_DATA_FLOW_WINDOW_SIZE 4 // resize in basis of strictly necessary
 
+typedef struct {
+    ptr ptr;
+    fa_ptr offset;
+    uint8_t size;
+} fa_JobDataFlow
+
 typedef uint32_t jobId_t;
 
 typedef struct {
     jobId_t id;
 
-    // The data flow window is basically a FIFO pointers register
-    ptr data_flow[FA_JOB_DATA_FLOW_WINDOW_SIZE];
+    // The data flow window is basically a FIFO pointers register of pointers in use
+    fa_JobDataFlow dataFlowWindow[FA_JOB_DATA_FLOW_WINDOW_SIZE];
+    uint8_t dataFlowOffset;
 
 } fa_Job;
