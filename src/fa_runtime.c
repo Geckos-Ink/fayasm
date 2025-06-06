07f5e38109d6949927424e7130e82054f3f28cfc
Riccardo Cecchini <rcecchini.ds@gmail.com>
2025-06-06 16:32:53 +0200

    runtime basics

---
+++ src/fa_runtime.c
@@ -0,0 +1,16 @@
+#include "fa_runtime.h"
+
+ptr fa_default_malloc(int size){
+    return malloc(size);
+}
+
+void fa_default_free(ptr region){
+    free(region);
+}
+
+fa_Runtime fa_Runtime_init(){
+    fa_Runtime* runtime = malloc(sizeof(fa_Runtime))
+    runtime->malloc = fa_default_malloc;
+    runtime->free = fa_default_free;
+    return runtime;
+}
--- src/fa_runtime.h
+++ src/fa_runtime.h
@@ -1,3 +1,14 @@
 #pragma once
 
 #include "fa_types.h"
+
+typedef ptr (*fa_Malloc)(int);
+typedef void (*fa_Free)(ptr);
+
+// Dichiara la struct utilizzando l'alias
+struct fa_Runtime {
+    fa_Malloc malloc;
+    fa_Free free;
+};
+
+fa_Runtime fa_Runtime_init();
