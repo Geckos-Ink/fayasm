8f007b4ba6acebd4c3138cb52c35474f5ca7c1f7
Riccardo Cecchini <rcecchini.ds@gmail.com>
2025-06-10 21:59:46 +0200

    new approach again

--- src/fa_job.c
+++ src/fa_job.c
@@ -6,7 +6,7 @@ fa_Job* fa_Job_init(){
     return job;
 }
 
-fa_JobDataFlow* fa_JobDataFlow_data_push(fa_Job* job, int size){
+/*fa_JobDataFlow* fa_JobDataFlow_data_push(fa_Job* job, int size){
     uint8_t offset = ++job.dataFlowOffset % FA_JOB_DATA_FLOW_WINDOW_SIZE;
     fa_JobDataFlow* data = &job.dataFlowWindow[offset];
 
@@ -19,4 +19,31 @@ fa_JobDataFlow* fa_JobDataFlow_data_push(fa_Job* job, int size){
 
 fa_JobDataFlow* fa_JobDataFlow_data_pull(fa_Job* job){
     return &job.dataFlowWindow[job.dataFlowOffset-- % FA_JOB_DATA_FLOW_WINDOW_SIZE];
+}*/
+
+fa_JobDataFlow* fa_JobDataFlow_init(){
+    fa_JobDataFlow* data = malloc(sizeof(fa_JobDataFlow));
+    data->ptr = NULL;
+    data->size = 0;
+    data->follows = NULL; // .. just use calloc
+    return data;
+}
+
+void fa_JobDataFlow_concat(fa_JobDataFlow* data, fa_JobDataFlow* to){
+    fa_JobDataFlow* cur = to;
+    while(cur != NULL){
+        if(cur->follows == NULL){
+            cur->follows = data;
+            break;
+        }
+        cur = cur->follows;
+    }
+}
+
+fa_JobDataFlow* fa_JobDataFlow_pull(fa_JobDataFlow* from){
+    fa_JobDataFlow* cur = from;
+    while(cur->follows != NULL){
+        cur = cur->follows;
+    }
+    return cur;
 }
--- src/fa_job.h
+++ src/fa_job.h
@@ -4,20 +4,23 @@
 #define FA_JOB_DATA_FLOW_WINDOW_SIZE 4 // resize in basis of strictly necessary
 #define FA_JOB_DATA_FLOW_MAX_SIZE 8 // bytes
 
+typedef struct fa_JobDataFlow;
 typedef struct {
     ptr ptr;
-    uint8_t offset;
-    uint8_t size;    
+    uint8_t size;  
+    fa_JobDataFlow* follows;
 } fa_JobDataFlow;
 
 typedef uint32_t jobId_t;
 
+// unused
 typedef struct {
     ptr ptr;
     fa_ptr offset;
     uint8_t size;
 } fa_OpCode;
 
+// unused
 typedef struct {
     ptr ptr;
     fa_ptr offset;
@@ -28,12 +31,16 @@ typedef struct {
     jobId_t id;
 
     // The data flow window is basically a FIFO pointers register of pointers in use
-    uint8_t data[FA_JOB_DATA_FLOW_MAX_SIZE * FA_JOB_DATA_FLOW_WINDOW_SIZE];
-    fa_JobDataFlow dataWindows[FA_JOB_DATA_FLOW_WINDOW_SIZE];
-    uint8_t dataFlowOffset;
+    //uint8_t data[FA_JOB_DATA_FLOW_MAX_SIZE * FA_JOB_DATA_FLOW_WINDOW_SIZE];
+    //fa_JobDataFlow dataWindows[FA_JOB_DATA_FLOW_WINDOW_SIZE];
+    //uint8_t dataFlowOffset;
     //uint8_t dataFlowPtrOffset;
 
     fa_ptr instructionPointer; // what instruction address is executing
+
+    // pull and push registers
+    fa_JobDataFlow* pull;
+    fa_JobDataFlow* push;
 
 } fa_Job;
 
@@ -41,5 +48,9 @@ fa_Job* fa_Job_init();
 
 //todo: Determine a more clear mechanism about data flows
 // currently a dynamic pointer is done every time, making optimization useless
-fa_JobDataFlow* fa_JobDataFlow_data_push(fa_Job *job, int size);
-fa_JobDataFlow* fa_JobDataFlow_data_pull(fa_Job *job);
+//fa_JobDataFlow* fa_JobDataFlow_data_push(fa_Job *job, int size);
+//fa_JobDataFlow* fa_JobDataFlow_data_pull(fa_Job *job);
+
+fa_JobDataFlow* fa_JobDataFlow_init();
+void fa_JobDataFlow_concat(fa_JobDataFlow* data, fa_JobDataFlow* to);
+fa_JobDataFlow* fa_JobDataFlow_pull(fa_JobDataFlow* from);
