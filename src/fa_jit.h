756d934981919e867c5351bfe7b697e235993d8c
Riccardo Cecchini <rcecchini.ds@gmail.com>
2025-10-30 04:48:47 +0100

    Update fa_jit.h

--- src/fa_jit.h
+++ src/fa_jit.h
@@ -3,4 +3,4 @@
 #include "fa_types.h"
 #include "fa_ops.h"
 
-//todo: include 'n write everything needed for WASM execution
+//todo: include 'n write everything needed for WASM execution (fa_runtime [..])
