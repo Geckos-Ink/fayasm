51b66faae260ddc83ffd8410147ebadcb1586e25
Riccardo Cecchini <rcecchini.ds@gmail.com>
2025-10-30 01:38:01 +0100

    Update fa_runtime.c

--- src/fa_runtime.c
+++ src/fa_runtime.c
@@ -12,5 +12,8 @@ fa_Runtime* fa_Runtime_init(){
     fa_Runtime* runtime = malloc(sizeof(fa_Runtime));
     runtime->malloc = fa_default_malloc;
     runtime->free = fa_default_free;
+
+    //todo: runtime->jobs [...]
+
     return runtime;
 }
